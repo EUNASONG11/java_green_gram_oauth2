28593c3ae4105e4c6c64fb69097bb788baa7c52f
SongEunA <dmsdktt@gmail.com>
2024-12-16 13:17:58 +0900

    12월 16일

--- src/main/java/com/green/greengramver/config/jwt/TokenProvider.java
+++ src/main/java/com/green/greengramver/config/jwt/TokenProvider.java
@@ -2,19 +2,36 @@ package com.green.greengramver.config.jwt;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.green.greengramver.config.security.MyUserDetails;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.time.Duration;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Set;
 
-@RequiredArgsConstructor
 @Service
 public class TokenProvider {
     private final ObjectMapper objectMapper; // Jackson 라이브러리
     private final JwtProperties jwtProperties;
+    private final SecretKey secretKey;
+
+    public TokenProvider(ObjectMapper objectMapper, JwtProperties jwtProperties) {
+        this.objectMapper = objectMapper;
+        this.jwtProperties = jwtProperties;
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtProperties.getSecretKey()));
+    }
 
     // JWT 생성
     public String generateToken(JwtUser jwtUser, Duration expiredAt) { // jwtUser는 jwt 생성을 담는 것, expiredAt은 기간 설정
@@ -23,6 +40,7 @@ public class TokenProvider {
     }
 
     private String makeToken(JwtUser jwtUser, Date expiry) {
+        // JWT 암호화
         return Jwts.builder()
                 .header().add("typ", "JWT")
                          .add("alg", "HS256")
@@ -31,7 +49,7 @@ public class TokenProvider {
                 .issuedAt(new Date())
                 .expiration(expiry)
                 .claim("signedUser", makeClaimByUserToString(jwtUser))
-                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .signWith(secretKey)
                 .compact();
     }
 
@@ -43,5 +61,33 @@ public class TokenProvider {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public boolean validToken(String token) {
+        try {
+            // JWT 복호화
+            getClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Authentication getAuthentication(String token) { //인증, 인가할 때 쓰는
+       UserDetails userDetails = getUserDetailsFromToken(token);
+       return userDetails == null ? null : new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+
+    public UserDetails getUserDetailsFromToken(String token) {
+        Claims claims = getClaims(token);
+        String json = (String)claims.get("signedUser");
+        JwtUser jwtUser = objectMapper.convertValue(json, JwtUser.class);
+        MyUserDetails userDetails = new MyUserDetails();
+        userDetails.setJwtUser(jwtUser);
+        return userDetails;
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseEncryptedClaims(token).getPayload();
     }
 }
