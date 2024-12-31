b7f37c0f430734bbad457724edde6bb76baab590
SongEunA <dmsdktt@gmail.com>
2024-12-31 11:12:39 +0900

    12월 31일

--- src/test/java/com/green/greengramver/feed/FeedPicMapperTest.java
+++ src/test/java/com/green/greengramver/feed/FeedPicMapperTest.java
@@ -1,5 +1,6 @@
 package com.green.greengramver.feed;
 
+import com.green.greengramver.TestUtils;
 import com.green.greengramver.feed.model.FeedPicDto;
 import com.green.greengramver.feed.model.FeedPicVo;
 import org.junit.jupiter.api.Test;
@@ -87,20 +88,45 @@ class FeedPicMapperTest {
 
         List<FeedPicVo> feedPicListAfter = feedPicTestMapper.selFeedPicListByFeedId(givenParam.getFeedId());
 
+        //feedPicListAfter에서 pic만 뽑아내서 이전처럼 List<String> 변형한 후 체크
+        List<String> feedOnlyPicList = new ArrayList<>(feedPicListAfter.size());
+        for (FeedPicVo feedPicVo : feedPicListAfter) {
+            feedOnlyPicList.add(feedPicVo.getPic());
+        }
+
+
         List<String> picList = Arrays.asList(pics);
         for (int i = 0; i < pics.length; i++) {
             String pic = picList.get(i);
-            System.out.printf("%s - contains: %b\n", pic, feedPicListAfter.contains(pic));
+            System.out.printf("%s - contains: %b\n", pic, feedOnlyPicList.contains(pic));
         }
 
-        assertAll(
-                  () -> {
+        //stream을 이용해서 한다.
+        //Predicate - 리턴 타입 O(boolean), 파라미터 O(FeedPicVo)
+        String[] pics2 = {"a.jpg", "b.jpg", "c.jpg", "d.jpg"};
 
-                  }
+        feedPicListAfter.stream().allMatch(feedPicVo -> picList.contains(feedPicVo.getPic()));
+
+
+        assertAll(
+                  () -> feedPicListAfter.forEach(feedPicVo -> TestUtils.assertCurrentTimeStamp(feedPicVo.getCreatedAt()))
                 , () -> assertEquals(givenParam.getPics().size(), actualAffectedRows)
                 , () -> assertEquals(0, feedPicListBefore.size())
                 , () -> assertEquals(givenParam.getPics().size(), feedPicListAfter.size())
-                , () -> assertTrue(feedPicListAfter.containsAll(Arrays.asList(pics)))
+                , () -> assertTrue(feedOnlyPicList.containsAll(Arrays.asList(pics)))
+                , () -> assertTrue(Arrays.asList(pics).containsAll(feedOnlyPicList))
+                , () -> assertTrue(feedPicListAfter.stream().allMatch(feedPicVo -> picList.contains(feedPicVo.getPic())))
+
+                , () -> assertTrue(feedPicListAfter.stream() // stream 생성
+                                                            .map(FeedPicVo::getPic) // 똑같은 크기의 새로운 반환 Stream<String> ["a.jpg", "b.jpg", "c.jpg"]
+                                                            .filter(pic -> picList.contains(pic)) // 필터는 연산의 결과가 true 인 것만 뽑아내서 새로운 stream 반환 Stream<String> ["a.jpg", "b.jpg", "c.jpg"]
+                                                            .limit(picList.size()) // stream 크기를 제한, 이전 stream의 크기가 10개인데 limit(2)로 하면 2개짜리 stream이 반환된다.
+                                                            .count() == picList.size())
+                , () -> assertTrue(feedPicListAfter.stream().map(FeedPicVo::getPic).toList().containsAll(Arrays.asList(pics)))
+                // Function return type 0, parameter 0 (FeedPicVo)
+                , () -> assertTrue(feedPicListAfter.stream().map(feedPicVo -> feedPicVo.getPic()) // ["a.jpg", "b.jpg", "c.jpg"]
+                                                   .toList() // stream > List
+                                                   .containsAll(Arrays.asList(pics)))
         );
     }
 
