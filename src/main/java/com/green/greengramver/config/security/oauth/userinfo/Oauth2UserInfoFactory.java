4060cee5792c808df860d10c07b36543a9bc5f40
SongEunA <dmsdktt@gmail.com>
2025-02-18 11:05:08 +0900

    구글 소셜 로그인 작업 완료

--- src/main/java/com/green/greengramver/config/security/oauth/userinfo/Oauth2UserInfoFactory.java
+++ src/main/java/com/green/greengramver/config/security/oauth/userinfo/Oauth2UserInfoFactory.java
@@ -16,6 +16,7 @@ public class Oauth2UserInfoFactory {
         return switch (signInProviderType){
             case KAKAO -> new KakaoOauth2UserInfo(attributes);
             case NAVER -> new NaverOAuth2UserInfo(attributes);
+            case GOOGLE -> new GoogleOAuth2UserInfo(attributes);
             default -> null;
         };
     }
